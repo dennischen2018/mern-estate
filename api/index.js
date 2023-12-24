096bcd49fb9e3295664dfdd0f863f0bcb0ccd1cf
Dennis Chen <dchenca@yahoo.com>
2023-12-23 20:54:08 -0500

    Create Sign Up Router

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -12,14 +13,16 @@ mongoose.connect(process.env.MONGO).then(() => {
 })
 
 const app = express();
+app.use(express.json())
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000 !!')
 });
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+// app.get('/', (req, res) => {
+//     res.send("Hello World!")
+// })
 
