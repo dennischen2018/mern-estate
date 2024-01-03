14f7309eff16e2974afe40b063c71e851a298c36
Dennis Chen <dchenca@yahoo.com>
2024-01-02 22:26:10 -0500

    create search ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/privateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
 import Listing from './pages/Listing'
+import Search from './pages/Search'
 
 
 export default function App() {
@@ -21,12 +22,12 @@ export default function App() {
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/listing/:listingId' element={<Listing />} />
       <Route path='/about' element={<About />} />
+      <Route path='/search' element={<Search />} />
       <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile />} />
         <Route path='/create-listing' element={<CreateListing/>} />
         <Route path='/update-listing/:listingId' element={<UpdateListing/>} />
       </Route>
     </Routes>
-    
   </BrowserRouter>
 }
