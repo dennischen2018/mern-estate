76753fb0e5cc799ca77505afa5eec0d154ee39f6
Dennis Chen <dchenca@yahoo.com>
2024-01-04 00:12:17 -0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,21 @@
+
+
 export default function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className="py-20 px-4 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-slate-800">About Dennis Estate</h1>
+        <p className="mb-4 text-slate-700">Dennis Estates is a leading real estate agency that specializes in helping 
+          clients buy, sell, and rent properties in the GTA area.
+
+        </p>
+        <p>
+
+        </p>
+        <p>
+
+        </p>
+      </div>
+    </div>
   )
 }
